0d76809a65c4da1789b1844d93f0d676056d72bd
Prantik <prantiknoor@gmail.com>
2021-11-21 13:23:57 +0600

    Point class created & its methods initialized

--- ecc.py
+++ ecc.py
@@ -1,3 +1,5 @@
+
+
 class FieldElement:
 
     def __init__(self, num, prime):
@@ -45,10 +47,78 @@ class FieldElement:
         num = (self.num * other.num**(self.prime-2)) % self.prime
         return self.__class__(num, self.prime)
 
-    def __pow__(self, power, modulo=None):
-        n = power % (self.prime - 1)
+    def __pow__(self, exponent, modulo=None):
+        # b^(p-1) = 1
+        # b^-e = b^-e * 1 = b^-e * b^p-1 = b^(p-1-e)
+        n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
+
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+
+        # Point infinity or zero
+        if self.x is None and self.y is None:
+            return
+
+        # Validating point
+        if self.y**2 != self.x**3 + self.a * self.x + self.b:
+            raise ValueError(f'({x}, {y}) is not on the curve')
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+                and self.a == other.a and self.b == other.b
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        return f'Point({self.x}, {self.y})_{self.a}_{self.b}'
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError(f'Points {self}, {other} are not on the same curve')
+
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+
+        # the line is in vertical
+        # x1 == x2 and y1 != y2
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        # when x1 != x2
+        # s = (y2 - y1)/(x2 - x1)
+        # x3 = s^2 - x1 - x2
+        # y3 = s(x1 - x3) - y1
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.y)
+            x3 = s**2 - self.x - other.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+
+        # when P1 == P2 and y = 0
+        # y = 0, so we cannot calculate slope > s = (3x^2 + a) / 2y
+        if self == other and self.y == 0:
+            return self.__class__(None, None, self.a, self.b)
+
+        # P1 == P2
+        # s = (3x^2 + a) / 2y
+        # x3 = s^2 - 2x
+        # y3 = s(x - x3) - y
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x3 = s**2 - 2 * self.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+
+        raise NotImplementedError
 
 
