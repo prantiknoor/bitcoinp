d7080608a8bd2d5ece586fefc155fea59712a07b
Prantik <prantiknoor@gmail.com>
2021-11-23 13:15:12 +0600

    test_rmul in ECCTest passed

--- test/ecc_test.py
+++ test/ecc_test.py
@@ -116,6 +116,44 @@ class ECCTest(TestCase):
             p3 = Point(x3, y3, a, b)
             self.assertEqual(p1 + p2, p3)
 
+    def test_rmul(self):
+        # tests the following scalar multiplications
+        # 2*(192,105)
+        # 2*(143,98)
+        # 2*(47,71)
+        # 4*(47,71)
+        # 8*(47,71)
+        # 21*(47,71)
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+
+        multiplications = (
+            # (coefficient, x1, y1, x2, y2)
+            (2, 192, 105, 49, 71),
+            (2, 143, 98, 64, 168),
+            (2, 47, 71, 36, 111),
+            (4, 47, 71, 194, 51),
+            (8, 47, 71, 116, 55),
+            (21, 47, 71, None, None),
+        )
+
+        # iterate over the multiplications
+        for s, x1_raw, y1_raw, x2_raw, y2_raw in multiplications:
+            x1 = FieldElement(x1_raw, prime)
+            y1 = FieldElement(y1_raw, prime)
+            p1 = Point(x1, y1, a, b)
+            # initialize the second point based on whether it's the point at infinity
+            if x2_raw is None:
+                p2 = Point(None, None, a, b)
+            else:
+                x2 = FieldElement(x2_raw, prime)
+                y2 = FieldElement(y2_raw, prime)
+                p2 = Point(x2, y2, a, b)
+
+            # check that the product is equal to the expected point
+            self.assertEqual(s * p1, p2)
+
 
 if __name__ == '__main__':
     main()
